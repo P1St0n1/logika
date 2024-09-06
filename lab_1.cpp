29cbdc4f10d50123710212dd805619a8d7fc2523
Serg <aaa@gmail.com>
2024-09-06 10:17:58 +0300

    Ex 2

--- lab_1.cpp
+++ lab_1.cpp
@@ -1,8 +1,12 @@
 ﻿#include <stdio.h>
 #include <limits.h>
+#include <random>
+#include <time.h>
 
 int main(void) {
 
+	  srand(time(0));
+	  //Задание 1
 	  int x[10] = { 0 };
 	  int min = INT_MAX, max = INT_MIN;
 
@@ -18,6 +22,16 @@ int main(void) {
 	  }
 	  printf("\nmax = %d min = %d", max, min);
 	  printf("\nresult = %d\n", max - min);
+
+	  //Задание 2
+	  int a[10] = { 0 };
+
+	  for (int i = 0; i < 10; i++) {
+			a[i] = rand() % 100;
+			printf("%d ", a[i]);
+	  }
+
+	  
 
 	  return 0;
 }
