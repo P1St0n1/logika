7105ccf875681d8560b24f0403963c071a6a6d59
Serg <aaa@gmail.com>
2024-10-22 12:34:15 +0300

    Доп задания

--- lab_4.cpp
+++ lab_4.cpp
@@ -11,19 +11,17 @@ struct Node {
 };
 
 struct Node* root;
-int count = 0;
+int count = 0, first_level = -1, last_level = -1;
 
 void print_tree(struct Node* r, int l) {
 
-	  if (r == NULL)
-	  {
+	  if (r == NULL) {
 			return;
 	  }
 
 	  print_tree(r->right, l + 1);
-	  for (int i = 0; i < l; i++)
-	  {
-			printf(" ");
+	  for (int i = 0; i < l; i++) {
+			printf(" -");
 	  }
 
 	  printf("%d\n", r->data);
@@ -43,13 +41,10 @@ struct Node* find(struct Node *r, int l) {
 	  }
 }
 
-struct Node* CreateTree(struct Node* root, struct Node* r, int data)
-{
-	  if (r == NULL)
-	  {
+struct Node* CreateTree(struct Node* root, struct Node* r, int data) {
+	  if (r == NULL) {
 			r = (struct Node*)malloc(sizeof(struct Node));
-			if (r == NULL)
-			{
+			if (r == NULL) {
 				  printf("Ошибка выделения памяти");
 				  exit(0);
 			}
@@ -66,46 +61,51 @@ struct Node* CreateTree(struct Node* root, struct Node* r, int data)
 			return r;
 	  }
 
-	  if (find(r, data) == NULL) {
+	  //if (find(r, data) == NULL) {
 			if (data > r->data)
 				  CreateTree(r, r->left, data);
 			else
 				  CreateTree(r, r->right, data);
-	  }
-	  else
-			printf("Элемент уже был введен\n");
+	  //}
+	  /*else
+			printf("Элемент уже был введен\n");*/
 
 	  return root;
 }
 
-//int count_in(struct Node* r, int c) {
-//	  if (r->data == c) {
-//			count += 1;
-//			if (r->right != NULL)
-//				  count_in(r->right, c);
-//	  }
-//	  else if (r->data < c && r->left != NULL)
-//			count_in(r->left, c);
-//	  else if (r->data > c && r->right != NULL)
-//			count_in(r->right, c);
-//	  else
-//			return 0;
-//	  return count;
-//}
+int count_in(struct Node* r, int c, int level, int first_level, int last_level) {
+	  if (r->data == c) {
+			count += 1;
+			if (first_level == -1)
+				  first_level = level;
+			if (r->right != NULL)
+				  count_in(r->right, c, level + 1, first_level, last_level);
+			last_level = level;
+	  }
+	  else if (r->data < c && r->left != NULL)
+			count_in(r->left, c, level + 1, first_level, last_level);
+	  else if (r->data > c && r->right != NULL)
+			count_in(r->right, c, level + 1, first_level, last_level);
+	  else
+			return 0;
+	  if (r->left == NULL && r->right == NULL) {
+			printf("Первый раз введенное значение встречается на %d уровне\n", first_level);
+			printf("Последний раз введенное значение встречается на %d уровне\n", last_level);
+	  }
+	  return count;
+}
 
 int main(void) {
-	  setlocale(LC_ALL, "");
+	  setlocale(LC_ALL, "rus");
 	  int D, start = 1;
-	  /*int H;*/
+	  int H;
 
 	  root = NULL;
 	  printf("-1 - окончание построения дерева\n");
-	  while (start)
-	  {
+	  while (start) {
 			printf("Введите число: ");
 			scanf_s("%d", &D);
-			if (D == -1)
-			{
+			if (D == -1) {
 				  printf("Построение дерева окончено\n\n");
 				  start = 0;
 			}
@@ -119,8 +119,8 @@ int main(void) {
 	  /*scanf_s("%d", &D);
 	  find(root, D);*/
 
-	  /*scanf_s("%d", &H);
-	  printf("Введенное значение найдено %d раз", count_in(root, H));*/
+	  scanf_s("%d", &H);
+	  printf("Введенное значение найдено %d раз", count_in(root, H, 0, first_level, last_level));
 
 	  return 0;
 }
